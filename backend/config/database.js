02ed8b03ea5f6c28f85af7068b867f46b4af5dbe
Vinayak Banga <vinayakbanga22@gmail.com>
2024-01-08 22:47:52 +0530

    changes in package.json

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,27 @@
 
-const mongoose=require("mongoose")
-const connectDb=()=>{
-        mongoose.connect(process.env.MONGO_URL);
-mongoose.connection
-    .once('open', function () {
-      console.log('MongoDB running');
-    })
-    .on('error', function (err) {
-      console.log(err);
-    });
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URL
 
-    }
-module.exports=connectDb
+const connectDb = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+module.exports = connectDb;
+// const mongoose=require("mongoose")
+// const connectDb=()=>{
+//         mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connection
+//     .once('open', function () {
+//       console.log('MongoDB running');
+//     })
+//     .on('error', function (err) {
+//       console.log(err);
+//     });
+
+//     }
+// module.exports=connectDb

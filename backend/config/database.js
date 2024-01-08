b64e7b88c7a748dbed87e2a76fb82e5174e40e4c
Vinayak Banga <vinayakbanga22@gmail.com>
2024-01-08 22:55:38 +0530

    Chnages in connectdb

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,27 +1,14 @@
 
-const mongoose = require('mongoose');
-const uri = process.env.MONGO_URL
+const mongoose=require("mongoose")
+const connectDb=()=>{
+        mongoose.connect(process.env.MONGO_URL);
+mongoose.connection
+    .once('open', function () {
+      console.log('MongoDB running');
+    })
+    .on('error', function (err) {
+      console.log(err);
+    });
 
-const connectDb = async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-  }
-};
-
-module.exports = connectDb;
-// const mongoose=require("mongoose")
-// const connectDb=()=>{
-//         mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connection
-//     .once('open', function () {
-//       console.log('MongoDB running');
-//     })
-//     .on('error', function (err) {
-//       console.log(err);
-//     });
-
-//     }
-// module.exports=connectDb
+    }
+module.exports=connectDb

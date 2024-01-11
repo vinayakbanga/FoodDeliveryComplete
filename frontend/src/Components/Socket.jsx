645194a542339e5d48e6471b109f0a3e603c3ae1
Vinayak Banga <vinayakbanga22@gmail.com>
2024-01-12 01:52:05 +0530

    Socket.io integrated bug fix

--- frontend/src/Components/Socket.jsx
+++ frontend/src/Components/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:4000/';
+const SOCKET_URL = 'http://localhost:4000/'||'https://fooddelivery-vywh.onrender.com/';
 const socket = io(SOCKET_URL, { transports: ['websocket'] });
 
 export default socket;
